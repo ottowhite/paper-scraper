c304c35158ca1952afb9f9773e83e0a4ae9198f9
White, Otto <otto.white20@imperial.ac.uk>
2025-04-21 14:58:18 +0100

    Add scraper for old SOSPs

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ from retrieve_paper_info import get_info_from_semantic_scholar, get_abstract_fro
 from dotenv import load_dotenv
 from scrape_osdi_atc_nsdi import scrape_sessions
 from scrape_sosp24 import scrape_sessions_sosp24
+from scrape_sosp_old import scrape_sessions_sosp_old
 
 def scrape_and_save(url, conference_name, filename_prefix):
     if conference_name == "atc" or conference_name == "nsdi" or conference_name == "osdi":
         data = scrape_sessions(url, conference_name)
     elif conference_name == "sosp24":
         data = scrape_sessions_sosp24(url)
+    elif conference_name == "sosp_old":
+        data = scrape_sessions_sosp_old(url)
 
     save_to_json(data, f"{filename_prefix}.json")
     save_to_notion_format(data, f"{filename_prefix}.notion.txt")
@@ -37,4 +40,4 @@ if __name__ == "__main__":
     # for year in range(20, 26):
     #     scrape_and_save(f"https://www.usenix.org/conference/nsdi{year}/technical-sessions", "nsdi", f"nsdi{year}_sessions")
 
-    scrape_and_save("https://www.usenix.org/conference/nsdi25/technical-sessions", "nsdi", "nsdi25_sessions")
+    scrape_and_save("https://sosp2023.mpi-sws.org/program.html", "sosp_old", "sosp23_sessions")
---
+++ scrape_sosp_old.py
@@ -0,0 +1,67 @@
+from bs4 import BeautifulSoup
+from retrieve_webpage import get_cached_webpage
+from retrieve_paper_info import get_info_from_semantic_scholar
+
+def flat_map(f, xs):
+    ys = []
+    for x in xs:
+        ys.extend(f(x))
+    return ys
+
+def scrape_sessions_sosp_old(url):
+    html_content = get_cached_webpage(url)
+    soup = BeautifulSoup(html_content, 'html.parser')
+    return parse_document_sosp_old(soup)
+
+def parse_document_sosp_old(soup):
+    program_div = soup.find('div', class_='program')
+
+    days_container_div = program_div.find_all('div', class_='col-md-12')[1]
+
+    table_divs = days_container_div.find_all('table', class_='table-spread')
+    table_rows = flat_map(lambda table_div: table_div.find_all('tr'), table_divs)
+
+    sessions = {}
+
+    i = 0
+    while i < len(table_rows) and "Closing" not in table_rows[i].text:
+        # Find the next session
+        while table_rows[i].get('class') != ['info']:
+            i += 1
+            if i >= len(table_rows):
+                break
+        
+        if i >= len(table_rows):
+            break
+
+        session_title = table_rows[i].find('strong').text.strip()
+
+        papers = []
+        # Get all papers in the session
+        # Every paper has a link, and then the authors follow it
+        while i < len(table_rows) and table_rows[i].get('class') != ['success']:
+            # If there is a link in this row, then it's a paper
+            if table_rows[i].find('a'):
+                # Get the paper name
+                paper_name = table_rows[i].find_all('a')[-1].text.strip()
+                paper_link = table_rows[i].find_all('a')[-1]['href']
+
+                # The authors follow it
+                i += 1
+                authors = table_rows[i].text.strip()
+
+                abstract, _ = get_info_from_semantic_scholar(paper_name)
+                papers.append({
+                    "title": paper_name,
+                    "authors": authors,
+                    "abstract": abstract,
+                    "link": paper_link,
+                })
+            
+            i += 1
+
+        # Get all papers in the session
+        sessions[session_title] = papers
+        i += 1
+
+    return sessions
