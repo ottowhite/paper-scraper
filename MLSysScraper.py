afb6ed3ba1dec5a55a6698efc9c19431332c3e2b
White, Otto <otto.white20@imperial.ac.uk>
2025-04-24 09:44:17 +0100

    Add start of neurips scraper to mlsysscraper

--- MLSysScraper.py
+++ MLSysScraper.py
@@ -6,6 +6,14 @@ from typing import List, Tuple, Dict
 from ConferenceScraper import ConferenceScraper
 
 class MLSysScraper(AbstractScrapingStrategy1):
+	def __init__(self, conference_name: str):
+		self.ignore_session_titles = [
+			"Poster",
+			"Overflow",
+			"Creative AI Session"
+		]
+		self.conference_name = conference_name
+
 	def extract_session_titles_and_links(self, top_level_soup: BeautifulSoup) -> List[Tuple[str, str]]:
 		timebox_divs = top_level_soup.find_all('div', class_='timebox')
 		session_title_divs = flat_map(lambda timebox_div: timebox_div.find_all('div', class_='sessiontitle'), timebox_divs)
@@ -15,10 +23,20 @@ class MLSysScraper(AbstractScrapingStrategy1):
 			session_title = session_title_div.text.strip()
 			session_title = session_title.split("\n")[0]
 
-			if "Poster" in session_title:
+			if any(ignore_title in session_title for ignore_title in self.ignore_session_titles):
 				continue
 
-			session_link = f"https://mlsys.org{session_title_div.find('a')['href']}"
+			if self.conference_name == "neurips24":
+				# Drop first two words of session title
+				session_title = " ".join(session_title.split(" ")[2:])
+
+			if self.conference_name == "mlsys24":
+				session_link = f"https://mlsys.org{session_title_div.find('a')['href']}"
+			elif self.conference_name == "neurips24":
+				session_link = f"https://neurips.cc{session_title_div.find('a')['href']}"
+			else:
+				raise ValueError(f"Unknown conference: {self.conference_name}")
+
 			sessions_and_links.append((session_title, session_link))
 
 		return sessions_and_links
@@ -46,10 +64,18 @@ class MLSysScraper(AbstractScrapingStrategy1):
 
 
 if __name__ == "__main__":
-	scraper = ConferenceScraper(
+	mlsys24_scraper = ConferenceScraper(
 		"mlsys24",
 		"https://mlsys.org/virtual/2024/calendar",
-		MLSysScraper()
+		MLSysScraper("mlsys24")
 	)
-	scraper.extract()
-	scraper.save_sessions(force_overwrite=False)
+	mlsys24_scraper.extract()
+	mlsys24_scraper.save_sessions(force_overwrite=False)
+
+	# neurips24_scraper = ConferenceScraper(
+	# 	"neurips24",
+	# 	"https://neurips.cc/virtual/2024/calendar",
+	# 	MLSysScraper("neurips24")
+	# )
+	# neurips24_scraper.extract()
+	# neurips24_scraper.save_sessions(force_overwrite=False)
