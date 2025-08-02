774305011c5f26f17029250a3ccb3450e9fc10af
White, Otto <otto.white20@imperial.ac.uk>
2025-08-03 00:55:09 +0100

    feat: Add a paper manager that handles saving and abstract population

---
+++ PaperManager.py
@@ -0,0 +1,112 @@
+from bs4 import BeautifulSoup
+from retrieve_webpage import get_cached_webpage_via_selenium
+from retrieve_paper_info import get_info_from_semantic_scholar
+import json
+import os
+from copy import deepcopy
+from utils import pretty_print_diff
+
+class PaperManager:
+	def __init__(self, papers, conference_name: str):
+		self.conference_name = conference_name
+		self.papers = papers
+
+	def populate_missing_abstracts_and_links(self):
+		for paper in self.papers:
+			self.try_populate_abstract_and_link_from_semantic_scholar(paper)
+			self.try_populate_missing_abstracts_from_doi(paper)
+
+	def try_populate_abstract_and_link_from_semantic_scholar(self, paper):
+		if paper["abstract"] == "":
+			abstract, link = get_info_from_semantic_scholar(paper["title"])
+			if abstract != "":
+				paper["abstract"] = abstract
+			if paper["link"] == "" and link != "":
+				paper["link"] = link
+
+	def try_populate_missing_abstracts_from_doi(self, paper):
+		if paper["abstract"] == "":
+			if paper["link"] != "" and "doi.org" in paper["link"]:
+				# If link in this format
+				# https://doi.org/10.1145/3676641.3716268
+				# Convert to this format
+				# https://dl.acm.org/doi/10.1145/3676641.3716268
+				if "doi.org" in paper["link"]:
+					dl_link = paper["link"].replace("doi.org", "dl.acm.org/doi")
+				elif "dl.acm.org" in paper["link"]:
+					dl_link = paper["link"]
+				else:
+					print(f"No doi found in link: {paper['link']}")
+					return
+
+				soup = BeautifulSoup(get_cached_webpage_via_selenium(dl_link), 'html.parser')
+
+				abstract = soup.find('section', id='abstract')
+				abstract_paragraphs = abstract.find_all('div', role='paragraph')
+				abstract_text = "\n".join([paragraph.text.strip() for paragraph in abstract_paragraphs])
+				paper["abstract"] = abstract_text
+
+	def print_stats(self):
+		total_papers = len(self.papers)
+		missing_abstracts = 0
+		missing_links = 0
+		for paper in self.papers:
+			if paper["abstract"] == "":
+				missing_abstracts += 1
+			if paper["link"] == "":
+				missing_links += 1
+
+		print(f"Conference: {self.conference_name} -----------------------------------------")
+		print(f"Papers: {total_papers}")
+		print(f"Missing abstracts: {missing_abstracts}")
+		print(f"Missing links: {missing_links}")
+	
+	def save_sessions(self, force_overwrite):
+		self.save_to_json(force_overwrite)
+		self.save_to_notion()
+
+	def save_to_json(self, force_overwrite=False):
+		"""Save the scraped data to a JSON file"""
+		filename = f"{self.conference_name}.json"
+
+		if not os.path.exists("json_format"):
+			os.makedirs("json_format")
+
+		# Create JSON content in memory
+		json_content = json.dumps(self.papers, indent=2, ensure_ascii=False)
+
+		# Read the existing JSON file if it exists
+		output_file_exists = os.path.exists(f"json_format/{filename}")
+		existing_content = ""
+		if output_file_exists:
+			with open(f"json_format/{filename}", 'r', encoding='utf-8') as f:
+				existing_content = f.read()
+		
+		if output_file_exists and not force_overwrite:
+			if existing_content != json_content:
+				# Print the diff
+				pretty_print_diff(existing_content, json_content)
+				exit()
+		
+		# Write the content to file
+		with open(f"json_format/{filename}", 'w', encoding='utf-8') as f:
+			f.write(json_content)
+
+		print(f"Data saved to json_format/{filename}")
+	
+	def save_to_notion(self):
+		"""Save the scraped data to a Notion format"""
+		filename = f"{self.conference_name}.notion.txt"
+
+		if not os.path.exists("notion_format"):
+			os.makedirs("notion_format")
+
+		with open(f"notion_format/{filename}", 'w', encoding='utf-8') as f:
+			for paper in self.papers:
+				temp_paper = deepcopy(paper)
+				f.write(f" - {paper['title']}\n")
+				temp_paper.pop("title")
+				for key, value in temp_paper.items():
+					f.write(f"   - {key}: {value}\n")
+		
+		print(f"Data saved to notion_format/{filename}")
